2a92632adb5305838a94f16f453482c5200bc8da
jbltx <gtmicka@hotmail.fr>
2015-06-20 04:35:56 +0200

    testing iOS support...

---
+++ OpenSpin-iOS/OpenSpin-iOS.h
@@ -0,0 +1,19 @@
+//
+//  OpenSpin-iOS.h
+//  OpenSpin-iOS
+//
+//  Created by Mickael Bonfill on 20/06/2015.
+//  Copyright (c) 2015 Mickael Bonfill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenSpin-iOS.
+FOUNDATION_EXPORT double OpenSpin_iOSVersionNumber;
+
+//! Project version string for OpenSpin-iOS.
+FOUNDATION_EXPORT const unsigned char OpenSpin_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenSpin_iOS/PublicHeader.h>
+
+
