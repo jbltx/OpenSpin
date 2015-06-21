5ac861ebc94537d4693afe45741e40d01081d073
jbltx <gtmicka@hotmail.fr>
2015-06-21 21:41:56 +0200

    testing iOS on CMake

---
+++ src/LoadImage.h
@@ -0,0 +1,27 @@
+#ifndef LOADIMAGE_H
+#define LOADIMAGE_H
+
+#include <SDL.h>
+
+class LoadImage
+{
+public:
+	LoadImage(SDL_Window *window,SDL_Renderer *renderer);
+	~LoadImage();
+
+	bool loadFromSurface(SDL_Surface *surface);
+	void render(SDL_Rect *clip, SDL_Rect *renderQuad, 
+						double angle, SDL_Point *center, SDL_RendererFlip flip);
+
+private:
+	SDL_Texture *m_texture;
+	int m_imgWidth;
+	int m_imgHeight;
+
+	SDL_Window *m_window;
+	SDL_Renderer *m_renderer;
+
+	void cleanUp();
+};
+
+#endif // LOADIMAGE_H
